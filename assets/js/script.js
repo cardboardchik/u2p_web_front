575a116fe025206f3cff539923d63a41f9784963
Karton4ik <artyrzzo9@gmail.com>
2023-11-29 19:36:57 +0600

    deploy_option

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,35 +9,6 @@ $(document).ready(function () {
     // feather icons
     feather.replace();
 
-    // smooth scroll
-    var scroll = new SmoothScroll('a[href*="#"]');
-
-    // tiny slider
-    $('#slider-1').slick({
-        infinite: true,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-    });
-
-    $('#slider-2').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        centerMode: true,
-        customPaging: function (slider, i) {
-            return '<div class="bg-white br-round w-1 h-1 opacity-50 mt-5" id=' + i + '> </div>'
-        },
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1
-            }
-        }, ]
-    });
 });
 
 const texts = ['easier', 'safer', 'more interesting', 'more fun']
